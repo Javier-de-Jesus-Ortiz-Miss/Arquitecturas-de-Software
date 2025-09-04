c61b28e0fe102d9f9db5d5a22b9c5c95a45ec199
HectorBarrera13 <hectoralejandro.bc@gmail.com>
2025-09-04 16:41:27 -0600

    Actualizacion de Application y creacion de Visualizer

--- entregas/entrega1/Java/entrega/Application.java
+++ entregas/entrega1/Java/entrega/Application.java
@@ -1,7 +1,20 @@
 package entrega;
 
 public class Application {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public Reader reader;
+    public NameSorter nameSorter;
+    public Formatter formatter;
+    public Visualizer visualizer;
+
+    public void main(String[] args) {
+        reader = new Reader();
+        nameSorter = new NameSorter();
+        formatter = new Formatter();
+        visualizer = new Visualizer();
+
+        List<String> names = reader.read("names.txt");
+        List<String> sortedNames = nameSorter.sort(names);
+        List<String> formattedNames = formatter.format(sortedNames);
+        visualizer.showResults(formattedNames);
     }
 }
